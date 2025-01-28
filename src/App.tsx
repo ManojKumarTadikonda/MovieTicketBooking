9aeb9713f3ae0487ebb4fcc670c04646700904f0
ManojKumarTadikonda <manojtadikonda5@gmail.com>
2025-01-28 16:00:06 +0530

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 import MoviesPage from './pages/MoviesPage';
 import BookingPage from './pages/BookingPage';
 
+
 function App() {
   return (
     <Router>
