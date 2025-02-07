194627995705256efaf5a735c3838e9bc1db77cb
Manoj38519 <n210519@gmail.com>
2025-02-07 09:53:28 +0530

    backend

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { bookTicket, getUserTickets } = require('../controllers/ticketController');
+const { bookTicket, getUserTickets,getBookedSeats } = require('../controllers/ticketController');
 const router = express.Router();
 
+router.get('/getseats',getBookedSeats);
 router.post('/book', bookTicket);
-router.get('/user/:userId', getUserTickets);
+// router.get('/user/:userId', getUserTickets);
 
 module.exports = router;
